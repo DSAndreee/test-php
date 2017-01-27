2fa6ddb708a57fcdf74b56a3827463fc56341234
DSAndreee <andre.da-silva@etudiant.univ-reims.fr>
2017-01-27 11:44:21 +0100

    Added FacturationLines tests

--- tests/Facturation/FacturationTest.php
+++ tests/Facturation/FacturationTest.php
@@ -13,4 +13,20 @@ class FacturationTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($facture->getId(), "2");
     }
    
+     public function testTotal(){
+        $facture = new Facturation(1,"Boulangerie Bonpain");
+        $facture->addLines("Création du site internet",1,438,20);
+        $facture->addLines("Formation aux réseaux sociaux",2,140,20);
+        $facture->addLines("Refonte graphique + Logo",1,930,20);
+        $this->assertEquals($facture->getTotal(), 1977.6);
+    }
+    public function testAddLines(){
+         $facture = new Facturation(1,"Boulangerie Bonpain");
+         $facture->addLines("Création du site internet",1,438,20);
+         foreach($facture->getLines() as $line){
+            $val = $line->getValue();
+         }
+         $this->assertEquals($val, 438);
+    }
+   
 }
